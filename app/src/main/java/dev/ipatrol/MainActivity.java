25bb4289f16f58dcddee672ac716f30ac5738d0c
Justin van Heek <Justin.vanHeek@mytwu.ca>
2017-06-14 13:53:48 -0700

    Initial commit

---
+++ app/src/main/java/dev/ipatrol/MainActivity.java
@@ -0,0 +1,37 @@
+package dev.ipatrol;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.new_patrol_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                goToNewPatrol();
+            }
+
+        });
+
+    }
+
+    private void goToNewPatrol() {
+        Intent intent = new Intent(this, NewPatrol.class);
+        startActivity(intent);
+
+    }
+
+
+
+}
---
+++ app/src/main/java/dev/ipatrol/NewPatrol.java
@@ -0,0 +1,13 @@
+package dev.ipatrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewPatrol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_patrol);
+    }
+}
